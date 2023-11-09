2639ee375f465854187b292647d301983fae9eba
ericjwittrock <ericjwittrock@gmail.com>
2023-11-08 18:01:34 -0600

    forgot what I changed

--- assignment02/initial_app/src/index.js
+++ assignment02/initial_app/src/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let cartList = JSON.parse(localStorage.getItem("cartList")) || {};
 
 function Header() {
   return (
@@ -46,19 +47,23 @@ function SearchBar() {
 
 function Card({id, src, name, txt, price}) {
 
-  const [count, setCount] = useState("0");
+  const [count, setCount] = useState(cartList[id] || 0);
 
   function incClick () {
     console.log("inc clicked");
-    let c = ~~count;
-    setCount(c+1);
+    let c = (~~count)+1;
+    setCount(c);
+    cartList[id] = c;
+    localStorage.setItem("cartList", JSON.stringify(cartList));
   }
 
   function decClick () {
     console.log("dec clicked");
-    let c = ~~count;
-    if (c > 0) {
-      setCount(c-1);
+    let c = (~~count)-1;
+    if (c >= 0) {
+      setCount(c);
+      cartList[id] = c;
+      localStorage.setItem("cartList", JSON.stringify(cartList));
     }
   }
 
@@ -66,9 +71,9 @@ function Card({id, src, name, txt, price}) {
   <div className="col">
     <div className="card shadow-sm">
       <h3 className="card-header">{name}</h3>
-      <img src={src} alt="shop image" />
+      <img src={src} alt="shop image" style={{margin: "5px"}} />
       <div className="card-body">
-        <span className="card-price" style={{border: '5px solid rgba(0, 100, 0, 1)', color: 'white', backgroundColor: 'green'}}>${price}.00</span>
+        <span className="card-price" style={{border: '4px solid rgb(60 175 60)', color: 'white', backgroundColor: "rgb(100 193 125)", borderRadius:"10px"}}>${price}.00</span>
         <p className="card-text">{txt}</p>
         <div className="d-flex justify-content-between align-items-center">
           <div className="btn-group">
@@ -124,7 +129,12 @@ function StorePage() {
     <div className="album py-5 bg-body-tertiary">
       <div className="container">
       <SearchBar/>
-      <a href="#" className="btn btn-primary my-2" onClick={changePageCart}>Cart/Checkout</a>
+      <button href="#" className="btn btn-primary my-2" onClick={changePageCart}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+        <span> Checkout</span>
+      </button>
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           <CardList />
         </div>
@@ -134,7 +144,41 @@ function StorePage() {
   );
 }
 
+function CartCard({src, name, price, quantity}) {
+  return (
+    <div className="card mb-3">
+    <div className="card-body">
+      <div className="d-flex justify-content-between">
+        <div className="d-flex flex-row align-items-center">
+          <div style={{width:"60%"}}>
+            <img
+              src={src}
+              className="img-fluid rounded-3" alt="Shopping item"/>
+          </div>
+          <div className="ms-3">
+            <h5>{name}</h5>
+            <div style={{border: '3px solid rgb(72 191 255)', textAlign:"center", padding:"3px", color: 'white', backgroundColor: "rgb(95 200 247)", borderRadius:"10px", fontWeight:"800"}}>
+              <span className="small mb-0">Quantity: {quantity}</span>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex flex-row align-items-center">
+          <div style={{width: "80px;"}}>
+            <h5 className="mb-0">${price}</h5>
+          </div>
+          <a href="#!" style={{color: "#cecece;"}}><i className="fas fa-trash-alt"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+
 function CartPage(){
+
+  let data = window.data.filter(i=>(cartList[i.id] > 0));
+
   function changePageStore(){
     root.render(
       <>
@@ -150,14 +194,131 @@ function CartPage(){
       </>
     )
   }
+
   return (
     <main>
-    <Header />
-    <a href="#" className="btn btn-primary my-2" onClick={changePageStore}>Back to Store</a>
-    <a href="#" className="btn btn-primary my-2" onClick={changePageConfirm}>Place Order</a>
-    <div>Cart</div>
+    <Header />    
+    <section className="h-100 h-custom" style={{backgroundColor: "#eee;"}}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="card">
+              <div className="card-body p-4">
+
+                <div className="row">
+
+                  <div className="col-lg-7">
+                    <button className="btn btn-primary my-2" onClick={changePageStore}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                      </svg>
+                      <span> Back to Store</span>
+                    </button>
+
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <div>
+                        <h3 className="mb-1">Shopping cart</h3>
+                        <p className="mb-0">You have {data.length} items in your cart</p>
+                      </div>
+                    </div>
+
+                    {
+                    data.map(i=>(
+                      <CartCard key={i.id+"_checkout"} src={i.src} name={i.name} price={i.price} quantity={cartList[i.id]}/>
+                    ))
+                    }
+
+
+                  </div>
+                  <div className="col-lg-5">
+
+                    <div className="card bg-info bg-gradient text-white rounded-3">
+                      <div className="card-body">
+                        <div className="d-flex justify-content-between align-items-center mb-4">
+                          <h5 className="mb-0">Card details</h5>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
+                            <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
+                          </svg>
+                        </div>
+
+
+                        <form className="mt-4">
+                          <div className="form-outline form-white mb-4">
+                            <input type="text" id="typeName" className="form-control form-control-lg" siez="17"
+                              placeholder="Cardholder's Name" />
+                            <label className="form-label" for="typeName">Cardholder's Name</label>
+                          </div>
+
+                          <div className="form-outline form-white mb-4">
+                            <input type="text" id="typeText" className="form-control form-control-lg" siez="17"
+                              placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
+                            <label className="form-label" for="typeText">Card Number</label>
+                          </div>
+
+                          <div className="row mb-4">
+                            <div className="col-md-6">
+                              <div className="form-outline form-white">
+                                <input type="text" id="typeExp" className="form-control form-control-lg"
+                                  placeholder="MM/YYYY" size="7" id="exp" minlength="7" maxlength="7" />
+                                <label className="form-label" for="typeExp">Expiration</label>
+                              </div>
+                            </div>
+                            <div className="col-md-6">
+                              <div className="form-outline form-white">
+                                <input type="password" id="typeText" className="form-control form-control-lg"
+                                  placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" />
+                                <label className="form-label" for="typeText">Cvv</label>
+                              </div>
+                            </div>
+                          </div>
+
+                        </form>
+
+                        <hr className="my-4"/>
+
+                        <div className="d-flex justify-content-between">
+                          <p className="mb-2">Subtotal</p>
+                          <p className="mb-2">$4798.00</p>
+                        </div>
+
+                        <div className="d-flex justify-content-between">
+                          <p className="mb-2">Shipping</p>
+                          <p className="mb-2">$20.00</p>
+                        </div>
+
+                        <div className="d-flex justify-content-between mb-4">
+                          <p className="mb-2">Total(Incl. taxes)</p>
+                          <p className="mb-2">$4818.00</p>
+                        </div>
+
+                        <button type="button" className="btn btn-outline-light btn-block btn-lg" onClick={changePageConfirm}>
+                          <div className="d-flex justify-content-between">
+                            <span>${4818}.00 Confirm</span>
+                          </div>
+                        </button>
+
+                      </div>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
     </main>
   );
+  
 }
 
 function ConfirmPage(){
@@ -171,8 +332,11 @@ function ConfirmPage(){
   return (
     <main>
     <Header />
-    <a href="#" className="btn btn-primary my-2" onClick={changePageStore}>Back to Store</a>
-    <div>Confirm</div>
+
+
+
+
+
     </main>
   );
 }
