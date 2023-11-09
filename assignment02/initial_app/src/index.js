fa091c90b3369485588239c2940902418d4b7d7f
ericjwittrock <ericjwittrock@gmail.com>
2023-11-08 20:25:40 -0600

    price breakdown

--- assignment02/initial_app/src/index.js
+++ assignment02/initial_app/src/index.js
@@ -130,7 +130,7 @@ function StorePage() {
       <div className="container">
       <SearchBar/>
       <button href="#" className="btn btn-primary my-2" onClick={changePageCart}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
         <span> Checkout</span>
@@ -163,10 +163,10 @@ function CartCard({src, name, price, quantity}) {
           </div>
         </div>
         <div className="d-flex flex-row align-items-center">
-          <div style={{width: "80px;"}}>
+          <div style={{width: "80px"}}>
             <h5 className="mb-0">${price}</h5>
           </div>
-          <a href="#!" style={{color: "#cecece;"}}><i className="fas fa-trash-alt"></i></a>
+          <a href="#!" style={{color: "#cecece"}}><i className="fas fa-trash-alt"></i></a>
         </div>
       </div>
     </div>
@@ -174,10 +174,23 @@ function CartCard({src, name, price, quantity}) {
   )
 }
 
+function PriceItem({id, quantity}) {
+  let data = window.data;
+  let item = data.filter(i=>(i.id === id))[0];
+  
+  return (
+    <div className="d-flex justify-content-between">
+      <p className="mb-2">{item.name} x {quantity}</p>
+      <p className="mb-2">${item.price*quantity}.00</p>
+    </div>
+  ) 
+}
 
 function CartPage(){
 
   let data = window.data.filter(i=>(cartList[i.id] > 0));
+  let sumPrice = 0;
+  data.forEach(i=>{sumPrice += i.price*cartList[i.id]});
 
   function changePageStore(){ // clicked the back button
     root.render(
@@ -250,7 +263,8 @@ function CartPage(){
       address: address,
       city: city,
       zip: zip,
-      card: card
+      card: card,
+      cartstuff: JSON.parse(JSON.stringify(cartList))
     }
 
     root.render(
@@ -263,7 +277,7 @@ function CartPage(){
   return (
     <main>
     <Header />    
-    <section className="h-100 h-custom" style={{backgroundColor: "#eee;"}}>
+    <section className="h-100 h-custom" style={{backgroundColor: "#eee"}}>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col">
@@ -274,8 +288,8 @@ function CartPage(){
 
                   <div className="col-lg-7">
                     <button className="btn btn-primary my-2" onClick={changePageStore}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                       </svg>
                       <span> Back to Store</span>
                     </button>
@@ -293,7 +307,6 @@ function CartPage(){
                     ))
                     }
 
-
                   </div>
                   <div className="col-lg-5">
 
@@ -301,24 +314,23 @@ function CartPage(){
                       <div className="card-body">
                         <div className="d-flex justify-content-between align-items-center mb-4">
                           <h5 className="mb-0">Card details</h5>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-credit-card" viewBox="0 0 16 16">
                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
                             <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
                           </svg>
                         </div>
 
-
                         <form className="mt-4">
                           <div className="form-outline form-white mb-4">
                             <input type="text" id="typeName" className="form-control form-control-lg" siez="17"
                               placeholder="Cardholder's Name" />
-                            <label className="form-label" for="typeName">Full Name</label>
+                            <label className="form-label" htmlFor="typeName">Full Name</label>
                           </div>
 
                           <div className="form-outline form-white mb-4">
                             <input type="text" id="typeEmail" className="form-control form-control-lg" siez="17"
                               placeholder="your@email.com"/>
-                            <label className="form-label" for="typeEmail">Email</label>
+                            <label className="form-label" htmlFor="typeEmail">Email</label>
                           </div>
 
                           <hr className="my-4"/>
@@ -327,15 +339,15 @@ function CartPage(){
                             <div className="col-md-6">
                               <div className="form-outline form-white">
                                 <input type="text" id="typeState" className="form-control form-control-lg"
-                                  placeholder="Your State" size="7" maxlength="15" />
-                                <label className="form-label" for="typeState">State</label>
+                                  placeholder="Your State" size="7" maxLength="15" />
+                                <label className="form-label" htmlFor="typeState">State</label>
                               </div>
                             </div>
                             <div className="col-md-6">
                               <div className="form-outline form-white">
                                 <input type="text" id="typeAdr" className="form-control form-control-lg"
-                                  placeholder="Your Adress" size="1" />
-                                <label className="form-label" for="typeAdr">Address</label>
+                                  placeholder="Your Address" size="1" />
+                                <label className="form-label" htmlFor="typeAdr">Address</label>
                               </div>
                             </div>
                           </div>
@@ -345,14 +357,14 @@ function CartPage(){
                               <div className="form-outline form-white">
                                 <input type="text" id="typeCity" className="form-control form-control-lg"
                                   placeholder="Your City" size="7"/>
-                                <label className="form-label" for="typeCity">City</label>
+                                <label className="form-label" htmlFor="typeCity">City</label>
                               </div>
                             </div>
                             <div className="col-md-6">
                               <div className="form-outline form-white">
                                 <input type="text" id="typeZip" className="form-control form-control-lg"
-                                  placeholder="Your Zip" size="1" minlength="5" maxlength="5" />
-                                <label className="form-label" for="typeZip">Zip Code</label>
+                                  placeholder="Your Zip" size="1" minLength="5" maxLength="5" />
+                                <label className="form-label" htmlFor="typeZip">Zip Code</label>
                               </div>
                             </div>
                           </div>
@@ -361,32 +373,30 @@ function CartPage(){
 
                           <div className="form-outline form-white mb-4">
                             <input type="text" id="typeCard" className="form-control form-control-lg" siez="17"
-                              placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
-                            <label className="form-label" for="typeCard">Card Number</label>
+                              placeholder="1234 5678 9012 3457" minLength="19" maxLength="19" />
+                            <label className="form-label" htmlFor="typeCard">Card Number</label>
                           </div>
 
                         </form>
 
                         <hr className="my-4"/>
 
-                        <div className="d-flex justify-content-between">
-                          <p className="mb-2">Subtotal</p>
-                          <p className="mb-2">$4798.00</p>
-                        </div>
+                        {
+                          data.map(i=>(
+                            <PriceItem key={i.id+"_checkoutprice"} id={i.id} quantity={cartList[i.id]}/>
+                          ))
+                        }
 
-                        <div className="d-flex justify-content-between">
-                          <p className="mb-2">Shipping</p>
-                          <p className="mb-2">$20.00</p>
-                        </div>
+                        <hr className="my-4"/>
 
                         <div className="d-flex justify-content-between mb-4">
-                          <p className="mb-2">Total(Incl. taxes)</p>
-                          <p className="mb-2">$4818.00</p>
+                          <p className="mb-2">Total</p>
+                          <p className="mb-2">${sumPrice}.00</p>
                         </div>
 
                         <button type="button" className="btn btn-outline-light btn-block btn-lg" onClick={changePageConfirm}>
                           <div className="d-flex justify-content-between">
-                            <span>${4818}.00 Confirm</span>
+                            <span>${sumPrice}.00 Confirm</span>
                           </div>
                         </button>
 
@@ -414,21 +424,42 @@ function CartPage(){
 }
 
 function ConfirmPage(){
+  let data = window.data.filter(i=>(cartList[i.id] > 0));
+  let cartList2 = window.formstuff.cartstuff;
+  console.log("Cart wiped!");
+  cartList = {};
+  console.log("Purchase made!");
+
   function changePageStore(){
+    localStorage.setItem("cartList", "{}");
     root.render(
       <>
         <StorePage />
+        
       </>
     )
   }
   return (
     <main>
     <Header />
+    <div className="col-lg-7">
+      <button className="btn btn-primary my-2" onClick={changePageStore}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        </svg>
+        <span> Back to Store</span>
+      </button>
 
+      <h2>Thank you, {window.formstuff.username}</h2>
+      <h3>Purchase complete on card ****-****-****-{window.formstuff.card.substring(window.formstuff.card.length - 4)}</h3>
+      {
+      data.map(i=>(
+        <CartCard key={i.id+"_checkout"} src={i.src} name={i.name} price={i.price} quantity={cartList2[i.id]}/>
+      ))
+      }
 
-
-
-
+      
+    </div>
     </main>
   );
 }
